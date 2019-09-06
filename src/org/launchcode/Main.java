4726eb903205727e34168c63382f2d0ea9f934fa
wbvtler <wesleyb28@gmail.com>
2019-09-06 13:56:49 -0500

    update main to work with other comparators

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,8 +1,12 @@
 package org.launchcode;
 
+import org.launchcode.comparators.AreaComparator;
 import org.launchcode.comparators.NameComparator;
+import org.launchcode.comparators.PopulationComparator;
+import org.launchcode.comparators.StateComparator;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
 
@@ -11,7 +15,7 @@ public class Main {
         ArrayList<City> cities = CityData.loadData();
 
         // TODO - Use different comparators here
-        NameComparator comparator = new NameComparator();
+        Comparator comparator = new StateComparator();
         cities.sort(comparator);
 
         printCities(cities);
